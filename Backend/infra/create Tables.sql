926e3e47004c1d26236a7a907ed330e6f656da74
LeoPitanga <leonardopitanga@gmail.com>
2022-11-17 17:16:42 -0300

    Encaminhado

---
+++ Backend/infra/create Tables.sql
@@ -0,0 +1,35 @@
+
+CREATE TABLE IF NOT EXISTS apctf.activities
+(
+	activity_id VARCHAR (255) PRIMARY KEY,
+	instrucoesacesso TEXT,
+	instrucoesobjetivo TEXT,
+	act_flag VARCHAR (255),
+	dica1 TEXT,
+	dica2 TEXT,
+	dica3 TEXT
+)
+TABLESPACE pg_default;
+
+CREATE TABLE IF NOT EXISTS apctf.students
+(
+	invenira_std_id VARCHAR(255) PRIMARY KEY,
+	activity_id_fk VARCHAR(255) NOT NULL,
+	acessoAtividade BOOLEAN,
+	acessoInstrucoes BOOLEAN,
+	acessoObjetivo BOOLEAN,
+	acertouFlag BOOLEAN,
+	acessoDica1 BOOLEAN,
+	acessoDica2 BOOLEAN,
+	acessoDica3 BOOLEAN,
+	FOREIGN KEY (activity_id_fk)
+		REFERENCES apctf.activities (activity_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS apctf.activities
+    OWNER to postgres;
+
+ALTER TABLE IF EXISTS apctf.students
+    OWNER to postgres;
