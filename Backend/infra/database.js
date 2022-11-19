2a3a89bd38c6114da9d2af10fbf90b37d83b8da1
LeoPitanga <leonardopitanga@gmail.com>
2022-11-18 22:28:20 -0300

    aee

--- Backend/infra/database.js
+++ Backend/infra/database.js
@@ -1,10 +1,10 @@
 const pgp = require('pg-promise')();
 const db = pgp({
-	user: 'postgres',
-	password: '123321',
-	host: 'localhost',
+	user: 'fnnaebagxvrvug',
+	password: '2469ae22141e2b2c69aa17ea9490b5fd011b5c8f1f6a73011b9413662351922c',
+	host: 'ec2-34-247-72-29.eu-west-1.compute.amazonaws.com',
 	port: 5432,
-	database: 'apctf'
+	database: 'dbr9j78atsvn3f'
 });
 
 module.exports = db;
