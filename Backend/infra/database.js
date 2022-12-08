333d122af5fb2ea4d0ee21e499058cf6973edaac
LeoPitanga <leonardopitanga@gmail.com>
2022-12-08 15:43:40 -0300

    teste pg

--- Backend/infra/database.js
+++ Backend/infra/database.js
@@ -1,4 +1,5 @@
 const pgp = require('pg-promise')();
+console.log(process.env.POSTGRES_URI);
 const PGCONNECTION_URI = process.env.POSTGRES_URI || 'postgres://postgres:123321@localhost:5432/apctf';
 
 const mongoose = require('mongoose');
