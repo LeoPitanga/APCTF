9d9922df4b2924953efddd6827098aea2f010ec8
LeoPitanga <leonardopitanga@gmail.com>
2022-12-08 15:29:34 -0300

    PG working

--- Backend/data/databaseManager.js
+++ Backend/data/databaseManager.js
@@ -3,6 +3,6 @@ const DB = require('../infra/database');
 
 
 const postgresManagerFactory = new DB.PostgresManagerFactory();
-const postgresManager = postgresManagerFactory.getDatabaseManager('postgres','123321','localhost',5432,'apctf');
+const postgresManager = postgresManagerFactory.getDatabaseManager();
 
 module.exports = postgresManager;
